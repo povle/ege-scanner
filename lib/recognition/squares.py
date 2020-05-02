77c6ff3c801ee7dc382c717a283828ab5b026208
povle <pasha@blinov.co>
2020-05-03 01:35:55 +0300

    add get_square

--- lib/recognition/squares.py
+++ lib/recognition/squares.py
@@ -1,7 +1,10 @@
+square_width = 30.15 / 1240
+square_height = 44.2 / 1753
+
 def get_coordinates(size, task, square):
     h, w = size[:2]
-    DX = 30.15 * w / 1240
-    DY = 44.2 * h / 1753
+    DX = square_width * w
+    DY = square_height * h
     x0 = 101 * w / 1240
     y0 = 448 * h / 1753
 
@@ -14,3 +17,10 @@ def get_coordinates(size, task, square):
     y += (row // 5) * DY/3
 
     return (round(x), round(y))
+
+def get_square(img, task, square):
+    h, w = img.shape[:2]
+    x0, y0 = get_coordinates((h, w), task, square)
+    x1 = x0 + round(w * square_width)
+    y1 = y0 + round(h * square_height)
+    return img[y0:y1, x0:x1]
