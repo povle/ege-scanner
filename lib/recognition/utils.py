8bb5cc6e10b5af58acbb76249f6a7fb0bd421a5b
povle <pasha@blinov.co>
2020-05-03 00:50:01 +0300

    add lib/recognition/utils.py

---
+++ lib/recognition/utils.py
@@ -0,0 +1,24 @@
+import cv2
+
+def pad_to_size(img, size, black=True):
+    h, w = img.shape[:2]
+    if size[0] < w or size[1] < h:
+        raise ValueError
+
+    top = bottom = (size[1] - h) // 2
+    if top + bottom != size[1] - h:
+        top += 1
+
+    left = right = (size[0] - w) // 2
+    if left + right != size[0] - w:
+        left += 1
+
+    color = (0, 0, 0) if black else (255, 255, 255)
+    return cv2.copyMakeBorder(img, top, bottom, left, right,
+                              cv2.BORDER_CONSTANT, value=color)
+
+
+def pad_to_square(img, black=True):
+    h, w = img.shape[:2]
+    a = max(h, w)
+    return pad_to_size(img, (a, a), black)
